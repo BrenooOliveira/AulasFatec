a48d20a3b1cba8607967ff3a022e260c2bc23b42
Breno de Oliveira <brenooalves2003@gmail.com>
2025-03-27 23:21:08 -0300

    iniciando exercicios de ordenacao

---
+++ estruturaDados/13 - Ordenação e Busca/01 - Teste da função bsort.c
@@ -0,0 +1,34 @@
+/*
+ Crie a função exibe(), complete e execute o programa a seguir.
+
+*/
+
+#include <stdio.h>
+
+void troca(int v[], int i, int j) {
+    // troca i por j
+    int x = v[i]; // auxiliar
+    v[i] = v[j]; 
+    v[j] = x;
+}
+
+void bsort(int v[], int n) {
+    for(int i=1; i<n; i++) // para cada i no range do vetor (n)
+        for(int j=0; j<n-i; j++) // para cada j no range de n-i (ou seja, até chegar no indice que o valor de i esta)
+            if( v[j]>v[j+1] ) // se o indice de j for menor que o proximo j
+                troca(v,j,j+1); // troca j por j+1
+}
+
+void exibe(int v[],int n){
+    for (int i=0; i<n; i++){
+        printf("%d, ", v[i]);
+    }
+}
+
+
+int main(void) {
+    int v[10] = {83,31,91,46,27,20,96,25,96,80};
+    bsort(v,10);
+    exibe(v,10);
+return 0;
+}
