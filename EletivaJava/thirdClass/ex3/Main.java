33d9bf50554730c092e5f4f1c06733982d99bad9
breno.oliveira <brenooalves2003@gmail.com>
2025-08-26 22:31:05 -0300

    :hammer: best practices

--- EletivaJava/thirdClass/ex3/Main.java
+++ EletivaJava/thirdClass/ex3/Main.java
@@ -2,12 +2,11 @@ package thirdClass.ex3;
 
 public class Main {
     public static void main(String[] args) {
-        Fibonacci fibonacci = new Fibonacci();
 
         System.out.println("Sequência de Fibonacci até 10 termos:");
 
         for (int i = 0; i < 10; i++) {
-            System.out.print(fibonacci.fibonacci(i) + " ");
+            System.out.print(Fibonacci.fibonacci(i) + " ");
         }
     }
 }
