969eef30bda911465751c6f6a47d810e236afa64
breno.oliveira <brenooalves2003@gmail.com>
2025-09-25 23:38:39 -0300

    :sparkles: working on OS

--- JavaPOO/20250919/src/Main.java
+++ JavaPOO/20250919/src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        EstoquePecasDAO.dropTable();
-        EstoquePecasDAO.criarTabela(); // cria tabela, se nao existir
-
-        // insere alguns produtos
-        EstoquePecasDAO.inserir("peca1",1,1);
-        EstoquePecasDAO.inserir("peca2",2,2);
-
-        // lista os produtos
-        EstoquePecasDAO.listar();
+        // TODO: Abertura de uma nova OS com dados do cliente e do aparelho
     }
 }
--- JavaPOO/20250919/src/OrdemServico.java
+++ JavaPOO/20250919/src/OrdemServico.java
@@ -1,4 +1,19 @@
 
 public class OrdemServico {
+    private String id_os;
+    private String id_cliente;
+    private Double valor_total;
+    private String status;
+
+    public OrdemServico(String id_cliente, Double valor_total, String status) {
+        this.id_cliente = id_cliente;
+        this.valor_total = valor_total;
+        this.status = status;
+    }
+
+    static void abrirOrdemServico(String id_cliente, Double valor_total, String status) {
+        OrdemServico os = new OrdemServico(id_cliente, valor_total, status);
+        System.out.println("Ordem de Serviço aberta com sucesso!");
+    }
 
 }
